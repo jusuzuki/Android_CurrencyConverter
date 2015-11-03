d58cd66c8e254044fdb2ede8434c722f2179268f
Momo Ozawa & Juliana Suzuki <student@epicodus.com>
2015-11-03 10:41:34 -0800

    Alert user about network connectivity (if network is unavailable)

--- app/src/main/java/com/epicodus/currencyconverter/MainActivity.java
+++ app/src/main/java/com/epicodus/currencyconverter/MainActivity.java
@@ -1,8 +1,12 @@
 package com.epicodus.currencyconverter;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
@@ -27,33 +31,44 @@ public class MainActivity extends AppCompatActivity {
         String currencyUrl =  "https://currency-api.appspot.com/api/" +
         source + "/" + target + "." + format;
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(currencyUrl)
-                .build();
+        if(isNetworkAvailable()) {
 
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Request request, IOException e) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(currencyUrl)
+                    .build();
 
-            }
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
 
-            @Override
-            public void onResponse(Response response) throws IOException {
-                try {
-                    Log.v(TAG, response.body().string());
-                    if (response.isSuccessful()) {
-
-                    }
-                    else {
-                        alertUserAboutError();
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "Exception caught: ", e);
                 }
-            }
-        });
+
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                }
+            });
+        }
+        else {
+            Toast.makeText(this, "Network is unavailable", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
     }
 
     private void alertUserAboutError() {
